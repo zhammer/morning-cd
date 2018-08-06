e06bcd5f29e2521431e135fdeaf5658c387021e6
zhammer <zachary_hammer@alumni.brown.edu>
2018-08-05 21:28:56 -0400

    Add spotify access token lambda function

---
+++ morning_cd/delivery/lambda/spotify_access_token.py
@@ -0,0 +1,22 @@
+"""Aws lambda delivery for fetching a spotify access token."""
+
+import json
+import os
+import requests
+
+def fetch_access_token(client_id, client_secret):
+    response = requests.post(
+        'https://accounts.spotify.com/api/token',
+        auth=(client_id, client_secret),
+        data={'grant_type': 'client_credentials'}
+    )
+    return response.json()['access_token']
+
+def handler(event, context):
+    client_id = os.environ['SPOTIFY_CLIENT_ID']
+    client_secret = os.environ['SPOTIFY_CLIENT_SECRET']
+    access_token = fetch_access_token(client_id, client_secret)
+    return {
+        'statusCode': 200,
+        'body': json.dumps({'accessToken': access_token})
+    }
