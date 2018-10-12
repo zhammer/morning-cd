94c4199c8402cbf21107e190540bdddd81f34833
zhammer <zachary_hammer@alumni.brown.edu>
2018-10-12 19:29:41 -0400

    Add graphql api endpoints

--- web_client/src/services/api/index.js
+++ web_client/src/services/api/index.js
@@ -1,6 +1,24 @@
-import { fetchSpotifyAccessToken } from './morningCd';
+import {
+  fetchSpotifyAccessToken,
+  fetchSunlightWindows,
+  fetchListens,
+  submitListen
+} from './morningCd';
 import { searchTracks } from './spotify';
 import { withFetchAccessToken } from './util';
 
-const api = withFetchAccessToken(fetchSpotifyAccessToken, { searchTracks });
+
+const spotifyApi = withFetchAccessToken(fetchSpotifyAccessToken, { searchTracks });
+
+const morningCdApi = {
+  fetchListens,
+  submitListen,
+  fetchSunlightWindows
+};
+
+const api = {
+  ...spotifyApi,
+  ...morningCdApi
+};
+
 export default api;
