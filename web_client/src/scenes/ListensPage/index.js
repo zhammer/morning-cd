055f6fee2a9e4d4e7e7b9c51eb149714bc7ed562
Zach Hammer <zachary_hammer@alumni.brown.edu>
2018-12-23 17:10:02 -0500

    Add playlists link (#50)
    * Add open sans condensed font
    
    * Add style for playlists link
    
    * Add strongTeal color
    
    * Add playlists link to listens page

--- web_client/src/scenes/ListensPage/index.js
+++ web_client/src/scenes/ListensPage/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Column } from 'components/styles';
 import ListenDeck from './components/ListenDeck';
-import { Header } from './ListensPage.styles';
+import { Header, Sub } from './ListensPage.styles';
 
 const HEADER_TEXT = 'Here are the first pieces of music people listened to today, from all over the world.';
 const HEADER_TEXT_NO_LISTENS = 'Nobody posted a listen to morning.cd today. Check back here later tonight. Morning.cd works all around the world, and it’s daytime somewhere.';
+const PLAYLIST_TEXT = '[Listen to today\'s spotify playlist]';
+const PLAYLIST_TEXT_NO_LISTENS = '[Listen to previous spotify playlists]';
 
 const ListensPage = ({ listens, onLastListenVisible }) => (
   <Column>
     <Header>{listens.length > 0 ? HEADER_TEXT : HEADER_TEXT_NO_LISTENS}</Header>
+    <Sub href='https://open.spotify.com/user/8fueir54qwc1v07r1cdl3k4rx'
+         target='_blank'>
+      {listens.length > 0 ? PLAYLIST_TEXT : PLAYLIST_TEXT_NO_LISTENS}
+    </Sub>
     <ListenDeck listens={listens} onLastListenVisible={onLastListenVisible} />
   </Column>
 );
