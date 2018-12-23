807cfe521e3a4029b5b50a8c92827ccb42db9bda
zhammer <zachary_hammer@alumni.brown.edu>
2018-12-23 17:06:28 -0500

    Add style for playlists link

--- web_client/src/scenes/ListensPage/ListensPage.styles.js
+++ web_client/src/scenes/ListensPage/ListensPage.styles.js
@@ -8,9 +8,32 @@ export const Header = withIsDaySundialConsumer(styled('div')`
 
   margin: 0 auto;
   text-align: center;
-  padding: .75em 0;
+  padding: .75em 0 .1em;
 
   @media (min-width: 35em) {
-    padding: 1em 0;
+    padding: 1em 0 .1em;
+  }
+`);
+
+
+export const Sub = withIsDaySundialConsumer(styled('a')`
+  color: ${props => props.isDay ? colors.teal : colors.lightTeal};
+  transition: color .2s linear;
+  text-align: center;
+  font-family: 'Open Sans Condensed', sans-serif;
+  font-size: .6em;
+  text-decoration: none;
+  display: block;
+  padding-bottom: .75em;
+
+
+  @media (min-width: 35em) {
+    font-size: .5rem;
+    padding-bottom: .9em;
+  }
+
+  &:hover {
+    cursor: pointer;
+    color: ${props => props.isDay ? colors.strongTeal : colors.teal};
   }
 `);
