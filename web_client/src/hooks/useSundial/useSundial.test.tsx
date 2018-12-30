c2841dc8c9c3a04c80626d061abd083ea4c88c70
zhammer <zachary_hammer@alumni.brown.edu>
2018-12-30 08:34:26 -0500

    Add full cycle tests

--- web_client/src/hooks/useSundial/useSundial.test.tsx
+++ web_client/src/hooks/useSundial/useSundial.test.tsx
@@ -8,7 +8,7 @@ import { timeout } from 'q';
 describe('component using useSundial', () => {
 
   it('calibrates to day during the day', async () => {
-    const delorean = createDelorean(new Date('August 4, 12:30:00'));
+    const delorean = createDelorean(new Date('August 4, 2001 12:30:00'));
     const sundialCallbackMock = createSundialCallbackMock();
 
     const { container } = render(
@@ -22,7 +22,7 @@ describe('component using useSundial', () => {
   });
 
   it('calibrates to night before sunrise', async () => {
-    const delorean = createDelorean(new Date('August 4, 05:30:00'));
+    const delorean = createDelorean(new Date('August 4, 2001 05:30:00'));
     const sundialCallbackMock = createSundialCallbackMock();
 
     const { container } = render(
@@ -36,7 +36,7 @@ describe('component using useSundial', () => {
   });
 
   it('calibrates to night after sunrise', async () => {
-    const delorean = createDelorean(new Date('August 4, 21:30:00'));
+    const delorean = createDelorean(new Date('August 4, 2001 21:30:00'));
     const sundialCallbackMock = createSundialCallbackMock();
 
     const { container } = render(
@@ -49,19 +49,67 @@ describe('component using useSundial', () => {
     expect(sundialCallbackMock.calls).toEqual(['onCalibrateToNight']);
   });
 
+  it('does the full day cycle, baby!', async () => {
+    // issues with fakeTimers and react-testing-library right now
+    // https://stackoverflow.com/questions/51126786/jest-fake-timers-with-promises/51132058#51132058
+    // at the moment i'm just doing a hack where the day cycles super quickly (1 window a second).
+    // unfortunately, this means the test tkes ~5 seconds to run.
+    const delorean = createDelorean(new Date('August 4, 2001 23:59:59'));
+    const sundialCallbackMock = createSundialCallbackMock();
+
+    const { container } = render(
+      <SundialUser fetchSunlightWindows={fetchSunlightWindowsRapid} sundialCallbacks={sundialCallbackMock.callbacks} />
+    );
+    const message = container.firstChild;
+    expect(message).not.toBeUndefined();
+    message && expect(message.textContent).toBe('calibrating...');
+    await waitForElement(() => getByText(container, 'Night. Last Sunrise: ' + augustFourthRapid.sunrise));
+    expect(sundialCallbackMock.calls).toEqual(['onCalibrateToNight']);
+
+    delorean.zoomTo(new Date('August 5, 2001 00:00:00'));
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    await waitForElement(() => getByText(container, 'Night. Last Sunrise: ' + augustFourthRapid.sunrise));
+    expect(sundialCallbackMock.calls).toEqual(['onCalibrateToNight', 'onNewDay']);
+    
+    await waitForElement(() => getByText(container, 'Day. Last Sunrise: ' + augustFifthRapid.sunrise));
+    expect(sundialCallbackMock.calls).toEqual(['onCalibrateToNight', 'onNewDay', 'onSunrise']);
+
+    await waitForElement(() => getByText(container, 'Night. Last Sunrise: ' + augustFifthRapid.sunrise));
+    expect(sundialCallbackMock.calls).toEqual(['onCalibrateToNight', 'onNewDay', 'onSunrise', 'onSunset']);
+  })
+
 })
 
-async function fetchSunlightWindows(today: Date): Promise<SunlightWindows> {
-  const isoString = today.toISOString();
+async function fetchSunlightWindowsRapid(today: Date): Promise<SunlightWindows> {
   
-  if (isoString.startsWith('2001-08-04')) {
+  if (today.toString().startsWith('Sat Aug 04')) {
+    return {
+      yesterday: augustThird,
+      today: augustFourthRapid,
+      tomorrow: augustFifthRapid
+    }
+  }
+  else if (today.toString().startsWith('Sun Aug 05')) {
+    return {
+      yesterday: augustFourthRapid,
+      today: augustFifthRapid,
+      tomorrow: augustSixth
+    }
+  }
+  else {
+    throw `mock fetchSunlightWindowsRapid only supports 2001-08-04 or 2001-08-05, not ${today}`;
+  }
+}
+
+async function fetchSunlightWindows(today: Date): Promise<SunlightWindows> {
+  if (today.toString().startsWith('Sat Aug 04')) {
     return {
       yesterday: augustThird,
       today: augustFourth,
       tomorrow: augustFifth
     }
   }
-  else if (isoString.startsWith('2001-08-05')) {
+  else if (today.toString().startsWith('Sun Aug 05')) {
     return {
       yesterday: augustFourth,
       today: augustFifth,
@@ -89,8 +137,18 @@ const augustFifth: SunlightWindow = {
 }
 
 const augustSixth: SunlightWindow = {
-  sunrise: new Date('August 4, 2001 06:22:00'),
-  sunset: new Date('August 4, 2001 19:49:00')
+  sunrise: new Date('August 6, 2001 06:22:00'),
+  sunset: new Date('August 6, 2001 19:49:00')
+}
+
+const augustFourthRapid: SunlightWindow = {
+  sunrise: new Date('August 4, 2001 06:21:00'),
+  sunset: new Date('August 4, 2001 23:59:58')
+}
+
+const augustFifthRapid: SunlightWindow = {
+  sunrise: new Date('August 5, 2001 00:00:01'),
+  sunset: new Date('August 5, 2001 00:00:02')
 }
 
 interface SundialCallbackMock {
@@ -141,7 +199,7 @@ function createDelorean(initialTime: Date): Delorean {
     }
     currentTime = newTime;
     MockDate.set(currentTime.toString());
-    jest.advanceTimersByTime(millisecondsToZoom);
+    // jest.advanceTimersByTime(millisecondsToZoom);
   }
 
   return { zoomTo };
