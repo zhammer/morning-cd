cc95fdb334e84c23f4effd68cb7138d6adc22b72
zhammer <zachary_hammer@alumni.brown.edu>
2018-12-30 10:29:07 -0500

    Delete withSundial

--- web_client/src/components/withSundial/context.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import SundialContext from '../../hooks/useSundial/context';
-
-export const withSundialConsumer = mapSundialToProps => WrappedComponent => props => (
-  <SundialContext.Consumer>
-    {sundial => <WrappedComponent {...mapSundialToProps(sundial)} {...props} />}
-  </SundialContext.Consumer>
-);
