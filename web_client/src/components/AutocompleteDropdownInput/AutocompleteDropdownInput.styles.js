3e374982e8fa5d3aeabe86132e410861eea4a495
zhammer <zachary_hammer@alumni.brown.edu>
2018-12-30 23:57:32 -0500

    Delete original AutocompleteDropdownInput component

--- web_client/src/components/AutocompleteDropdownInput/AutocompleteDropdownInput.styles.js
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled/macro';
-
-export const FlexColumn = styled('div')`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  position: relative;
-`;
