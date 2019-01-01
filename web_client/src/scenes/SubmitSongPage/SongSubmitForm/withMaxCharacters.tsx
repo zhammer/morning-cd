ccf188d185beb814ea8bc5e8b76267b7c91aea5c
zhammer <zachary_hammer@alumni.brown.edu>
2018-12-31 20:13:36 -0500

    Update withMaxCharacters

--- web_client/src/scenes/SubmitSongPage/SongSubmitForm/withMaxCharacters.tsx
+++ web_client/src/scenes/SubmitSongPage/SongSubmitForm/withMaxCharacters.tsx
@@ -1,24 +1,24 @@
 import React, { Component, useState, HTMLProps } from 'react';
 import { Container, Counter } from './withMaxCharacters.styles';
 
+
+/**
+ * Add a maxCharacters limit and bottom-right text display on an input form.
+ * (This is pretty odd and probably shouldn't be an HOC. I'll clean up later.)
+ * (Also, the typing got confusing so I just set some things to `any`.)
+ * @param InputComponent Component where user is inputting text.
+ * @param maxCharacters Max characters of input string.
+ */
 function withMaxCharacters(InputComponent: React.ComponentType<any>, maxCharacters: number) {
   return function (props: any) {
     const [focused, setFocused] = useState(false);
-
-    function handleInputFocused() {
-      setFocused(true);
-    }
-
-    function handleInputBlurred() {
-      setFocused(false);
-    }
 
     return (
       <Container>
         <InputComponent
           maxLength={maxCharacters}
-          onFocus={handleInputFocused}
-          onBlur={handleInputBlurred}
+          onFocus={() => setFocused(true)}
+          onBlur={() => setFocused(false)}
           { ...props } />
         {focused && props.value && <Counter>{props.value.length}/{maxCharacters}</Counter>}
       </Container>
