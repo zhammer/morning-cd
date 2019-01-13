700d219e3fc44d0c96a72f5a9c67d408d3cd27fa
zhammer <zachary_hammer@alumni.brown.edu>
2019-01-13 14:44:28 -0500

    Add ShowNewListensButton style

--- web_client/src/scenes/ListensPage/ListenDeck.styles.tsx
+++ web_client/src/scenes/ListensPage/ListenDeck.styles.tsx
@@ -1,5 +1,10 @@
+/** @jsx jsx */
+import { jsx, css, keyframes } from '@emotion/core';
 import styled from '@emotion/styled/macro';
 import colors from '../../theme';
+import useIsDaySundialConsumer from '../../util/useIsDaySundialConsumer';
+
+jsx;  // https://github.com/emotion-js/emotion/issues/1112
 
 export const Separater = styled.hr`
   border: 0;
@@ -12,6 +17,29 @@ export const Separater = styled.hr`
   background-image: linear-gradient(to right, rgba(0, 0, 0, 0), ${colors.lightGray}, rgba(0, 0, 0, 0));
 `;
 
+export function ShowNewListensButton(props: React.HTMLProps<HTMLDivElement>) {
+  const isDay = useIsDaySundialConsumer();
+
+  return (
+    <div
+      css={css`
+        cursor: pointer;
+        text-align: center;
+        background: ${colors.lightTealTransparent};
+        transition: color 5s linear;
+        margin-bottom: .5em;
+        padding: .25em 0;
+        border-radius: .25em;
+        color: ${isDay ? colors.darkGray : colors.white};
+        font-size: .75em;
+        @media (min-width: 35em) {
+          font-size: .65em;
+        }
+      `}
+      {...props}
+      />
+  )
+}
 
 export const Container = styled.div`
   margin-bottom: .75em;
