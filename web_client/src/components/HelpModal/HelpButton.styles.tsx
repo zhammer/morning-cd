579405b87903c8563e0c64557872525382ba722c
zhammer <zachary_hammer@alumni.brown.edu>
2019-01-13 18:11:07 -0500

    Use css prop for help button

--- web_client/src/components/HelpModal/HelpButton.styles.tsx
+++ web_client/src/components/HelpModal/HelpButton.styles.tsx
@@ -1,27 +1,37 @@
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core'
 import styled from '@emotion/styled';
 import CircleSvg from './Circle.svg';
 import colors from '../../theme';
+import useIsDaySundialConsumer from '../../util/useIsDaySundialConsumer';
 
-interface IsDayProps {
-  isDay: boolean;
+jsx; // https://github.com/emotion-js/emotion/issues/1112
+
+export const Container = (props: React.HTMLAttributes<HTMLDivElement>) => {
+  const isDay = useIsDaySundialConsumer();
+
+  return (
+    <div
+      css={css`
+        position: fixed;
+        height: 2.5em;
+        width: 2.5em;
+        right: .5em;
+        bottom: .5em;
+        opacity: .5;
+        cursor: pointer;
+        color: ${isDay ? colors.mediumGray : colors.lightGray};
+        fill: ${isDay ? colors.mediumGray : colors.lightGray};
+        transition: color 5s linear, fill 5s linear, opacity .5s linear;
+
+        &:hover {
+          opacity: 1;
+        }
+      `}
+      {...props}
+    />
+  )
 }
-
-export const Container = styled.div<IsDayProps>`
-  position: fixed;
-  height: 2.5em;
-  width: 2.5em;
-  right: .5em;
-  bottom: .5em;
-  opacity: .5;
-  cursor: pointer;
-  color: ${props => props.isDay ? colors.mediumGray : colors.lightGray};
-  fill: ${props => props.isDay ? colors.mediumGray : colors.lightGray};
-  transition: color 5s linear, fill 5s linear, opacity .5s linear;
-
-  &:hover {
-    opacity: 1;
-  }
-`;
 
 
 export const Circle = styled(CircleSvg)`
--- web_client/src/components/HelpModal/HelpButton.tsx
+++ web_client/src/components/HelpModal/HelpButton.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import { Circle, Container, Text } from './HelpButton.styles';
-import useIsDaySundialConsumer from '../../util/useIsDaySundialConsumer';
 
 interface HelpButtonProps {
   onClick(): void;
 }
 
 function HelpButton({ onClick }: HelpButtonProps) {
-  const isDay = useIsDaySundialConsumer();
-
   return (
-    <Container isDay={isDay} onClick={onClick} >
+    <Container onClick={onClick} >
       <Text>?</Text>
       <Circle />
     </Container>
