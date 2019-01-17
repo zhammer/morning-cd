7bacc119b2aed6b0872a382898e3ff81b8fa56e1
zhammer <zachary_hammer@alumni.brown.edu>
2019-01-16 19:41:26 -0500

    Add body-scroll-lock package

--- web_client/src/declarations.d.ts
+++ web_client/src/declarations.d.ts
@@ -13,3 +13,18 @@ declare module '@emotion/css/macro' {
 declare module 'react-social-icons' {
     export function SocialIcon(props: any): any;
 }
+
+declare module 'body-scroll-lock' {
+  export interface BodyScrollOptions {
+    reserveScrollBarGap: boolean;
+  }
+
+  export function disableBodyScroll(
+    targetElement: HTMLElement,
+    options?: BodyScrollOptions
+  ): void;
+
+  export function enableBodyScroll(targetElement: HTMLElement): void;
+
+  export function clearAllBodyScrollLocks(): void;
+}
