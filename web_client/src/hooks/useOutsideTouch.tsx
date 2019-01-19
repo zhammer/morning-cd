fec3eb9e6713621d86349edbc76df8c77ee8406d
zhammer <zachary_hammer@alumni.brown.edu>
2019-01-19 11:08:11 -0500

    Use constants for touch start and touch end

--- web_client/src/hooks/useOutsideTouch.tsx
+++ web_client/src/hooks/useOutsideTouch.tsx
@@ -5,18 +5,21 @@ interface Handlers {
   onOutsideTouchEnd?: () => void;
 }
 
+const TOUCH_START = 'touchstart';
+const TOUCH_END = 'touchend';
+
 /**
  * Hook for easily responding to touchstart and touchend events from elements
  * 'outside' of a given html element.
  */
 export default function useOutsideTouch<T extends HTMLElement>(ref: MutableRefObject<T | null>, handlers: Handlers) {
   useEffect(() => {
-    document.addEventListener('touchstart', listener);
-    document.addEventListener('touchend', listener);
+    document.addEventListener(TOUCH_START, listener);
+    document.addEventListener(TOUCH_END, listener);
 
     return () => {
-      document.removeEventListener('touchstart', listener);
-      document.removeEventListener('touchend', listener);
+      document.removeEventListener(TOUCH_START, listener);
+      document.removeEventListener(TOUCH_END, listener);
     }
   }, []);
 
@@ -29,11 +32,11 @@ export default function useOutsideTouch<T extends HTMLElement>(ref: MutableRefOb
       return;
     }
 
-    if (event.type === 'touchstart') {
+    if (event.type === TOUCH_START) {
       handlers.onOutsideTouchStart && handlers.onOutsideTouchStart();
     }
 
-    if (event.type === 'touchend') {
+    if (event.type === TOUCH_END) {
       handlers.onOutsideTouchEnd && handlers.onOutsideTouchEnd();
     }
   }
