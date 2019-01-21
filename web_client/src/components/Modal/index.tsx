ea040cf1eeea73254a7ce42be3b7cf371bb0c995
Zach Hammer <zachary_hammer@alumni.brown.edu>
2019-01-21 16:31:14 -0500

    Cleanup hoisted exports (#113)

--- web_client/src/components/Modal/index.tsx
+++ web_client/src/components/Modal/index.tsx
@@ -1,16 +1,5 @@
 import Modal, { ModalProps as _ModalProps } from './Modal';
-import {
-  SectionDivider,
-  SectionTitle,
-  SectionBody
-} from './Modal.styles';
 
+export * from './Modal.styles';
 export default Modal;
-
-export interface ModalProps extends _ModalProps {};
-
-export {
-  SectionDivider,
-  SectionTitle,
-  SectionBody,
-};
+export type ModalProps = _ModalProps;
